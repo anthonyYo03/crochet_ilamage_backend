592b8193fdfabf2197787b2f297979f82f307670
anthonyYo03 <antoyou78@gmail.com>
2026-06-17 14:59:04 +0300

    test without using token

--- middlewares/config.js
+++ middlewares/config.js
@@ -1,4 +1,4 @@
-export const secretKey ='ecc7953bb87c02efdeed4baaaa9640efa2b9d93518eace55f9be56368fb5fd56';
+export const secretKey =`${process.env.SECRET_KEY}`;
 if (!secretKey) {
   console.error('❌ FATAL ERROR: SECRET_KEY not found in .env file');
   process.exit(1); // Stop the server immediately
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {productController} from "../controllers/product.controller.js";
-import { verifyToken } from "../middlewares/auth.js";
+// import { verifyToken } from "../middlewares/auth.js";
 // import { requireAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/getAll", productController.getAllProducts);
 router.get("/getOne/:id", productController.getOneProduct);
-router.post("/create", verifyToken, productController.createProduct);
-router.put("/edit/:id", verifyToken, productController.editProduct);
-router.delete("/delete/:id", verifyToken, productController.deleteProduct);
+router.post("/create", productController.createProduct);
+router.put("/edit/:id", productController.editProduct);
+router.delete("/delete/:id", productController.deleteProduct);
 
 export default router;
